0951e6c89fd82b35e06931c19fe3da8bf37b85ef
Robert Wright <wrig0410@algonquinlive.com>
2020-11-08 23:47:04 -0500

    first stab at des_inputs.c

--- src/des_inputs.c
+++ src/des_inputs.c
@@ -1,12 +1,14 @@
 /**
- * Nick Wright
- * 040911884
+ * Nick Wright & Kevin Luu
+ * 040911884 & 040905303
  * CST8244
  */
 
-#include "des.h"
+#include "../../des_controller/src/des.h"
 
 int main(void) {
+
+	Person person;
 
 	client_send_t msg_send;
 	server_response_t msg_receive;
@@ -14,10 +16,10 @@ int main(void) {
 	int coid;
 
 	//On Failure: print usage message and EXIT_FAILURE
-	if (argc != 5) {
+	if (argc != 2) {
 		//print usage message
 		fprintf(stderr,
-				"Usage: ./des_inputs <pid of des_display> x y z <enter>\n");
+				"Usage: ./des_inputs <pid of des_display> <enter>\n");
 		exit(EXIT_FAILURE);
 	}
 
@@ -36,32 +38,54 @@ int main(void) {
 
 	//Phase II
 	while( TRUE ){
-
+		char input[64];
 		//Prompt User for DES input-event (printf())
-
+		puts("Enter the event type (ls = left scan, rs = right scan, ws = weight scale, lo = left open, ro = right open, lc = left closed, rs = right closed, gru = guard right unlock, grl = guard right lock, gll = guard left lock, glu = guard left unlock, exit = exit programs)\n");
 		//Get input-event from User (scanf())
-
+		scanf("%s", input);
 		//IF input-event == "ls" THEN
+		if (strcmp(&input, inMessage[0]))
+		{
+			//Prompt User for person ID
+			printf("Enter the person's ID: \n");
+			//Get person ID from User
+			scanf("%d", &person.id);
 
-		//Prompt User for person ID
+		}
+		//apply what you know and repeat for "rs"
+		if (strcmp(&input, inMessage[1]))
+		{
+			//Prompt User for person ID
+			printf("Enter the person's ID: \n");
+			//Get person ID from User
+			scanf("%d", &person.id);
 
-		//Get person ID from User
+		}
+		//apply what you know and repeat for "ws", with the notable exception: prompt User for person's weight
+		if (strcmp(&input, inMessage[6]))
+		{
+			//Prompt User for person ID
+			printf("Enter the person's weight: \n");
+			//Get person ID from User
+			scanf("%d", &person.weight);
 
-		//TODO: apply what you know and repeat for "rs"
-
-		//TODO: apply what you know and repeat for "ws", with the notable exception: prompt User for person's weight
-
+		}
 		//Send Person object to controller (server); no message from controller is returned to client.
 
 		//IF input-event == "exit" THEN
-
-		//break out of while loop
-
+		if (strcmp(&input, inMessage[11]))
+		{
+			//Prompt User for person ID
+			printf("Exiting \n");
+			//TODO send message to controller without person?
+			//break out of while loop
+			break;
+		}
+		//go again they didn't type anything acceptable
 	}
 
 	//Phase III
 	//Call ConnectDetach() to detach from controller's channel
 	ConnectDetach(coid);
-
 	exit(EXIT_SUCCESS);
 }
